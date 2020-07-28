a3719cc3fa1c4c62a17e94e52e738534774a7caf
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2020-07-28 09:07:28 +0200

    Updates

---
+++ precache-manifest.87c93e25f9dac23048ac68846d7fa13f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efe55f82256bd74fc21ad7c3101566b",
+    "url": "/MA-portfolio/index.html"
+  },
+  {
+    "revision": "344fd384298bd5156408",
+    "url": "/MA-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9dc8a105155c572dbbb6",
+    "url": "/MA-portfolio/static/js/2.86227536.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MA-portfolio/static/js/2.86227536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344fd384298bd5156408",
+    "url": "/MA-portfolio/static/js/main.8842247a.chunk.js"
+  },
+  {
+    "revision": "0681e972f48a321cbf8d",
+    "url": "/MA-portfolio/static/js/runtime-main.10a112d2.js"
+  }
+]);
